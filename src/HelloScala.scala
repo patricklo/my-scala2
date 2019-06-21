17f66ee99caa299d05153b95eafde2861be9ed1f
Patrick Lo <patrick.lo@pinpointfund.com>
2019-06-21 16:08:58 +0800

    commit2

--- src/HelloScala.scala
+++ src/HelloScala.scala
@@ -1,6 +1,15 @@
 class HelloScala {
 
-  def sayHello(): Unit ={
-    
+  def sayHello(name:String, age: Int)  ={
+    if(age>18){printf("hi %s, you are a big boy!!!0",name)}
+    else printf("hi %s, you are little boy", name)
+  }
+}
+
+
+object Test{
+  def main(args: Array[String]): Unit ={
+    val pt = new HelloScala()
+    pt.sayHello("leo",100)
   }
 }
